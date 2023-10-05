7581bcd12c1ea1a6ce446fffc5f364ce13024524
Dominic Diederich <dominicdiederich@dominics-air.dhcp.nd.edu>
2023-10-05 16:02:38 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Parse from "parse";
 
+Parse.serverURL = 'https://parseapi.back4app.com'; // This is your Server URL
+// Remember to inform BOTH the Back4App Application ID AND the JavaScript KEY
+Parse.initialize(
+  'BCrUQVkk80pCdeImSXoKXL5ZCtyyEZwbN7mAb11f', // This is your Application ID
+  '4wPYRKbpTJeCdmFNaS31AiQZ8344aaYubk6Uo8VW' // This is your Javascript key
+);
 function App() {
   return (
     <div className="App">

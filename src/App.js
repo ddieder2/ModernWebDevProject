8f926d1dea076d9c3a4589738208dda69030cea2
Dominic Diederich <dominicdiederich@dominics-air.dhcp.nd.edu>
2023-10-10 17:13:21 -0400

    Update parse

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Components from './Components/Components';
 Parse.serverURL = 'https://parseapi.back4app.com'; // This is your Server URL
 // Remember to inform BOTH the Back4App Application ID AND the JavaScript KEY
 Parse.initialize(
-  'BCrUQVkk80pCdeImSXoKXL5ZCtyyEZwbN7mAb11f', // This is your Application ID
-  '4wPYRKbpTJeCdmFNaS31AiQZ8344aaYubk6Uo8VW' // This is your Javascript key
+  'vmhk69ASXetFNV66CXRxN4zZlSgQ7HeR53jaoeAV', // This is your Application ID
+  '9zPzMSb6SdBr5o3NqjFIlsLPLEo1ORJ48gW0D1Mc' // This is your Javascript key
 );
 function App() {
   return (
